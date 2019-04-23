bb639b3857ad0a08d33309cf9a7b3f966ec2aa7c
_Joe <joelin890120@gmail.com>
2019-04-24 00:48:08 +0800

    linebot108508

--- index.js
+++ index.js
@@ -396,7 +396,7 @@ bot.on('message', function (event) {
                         var i = arrCart.length
 
                         var a = arrCartAmt
-                        for(var k = 0; k<=2; k++){
+                        for(var k = 0; k<=1; k++){
                             if(arrCart[k][0]==a){
                                 arrCart[k][1]+=msg1;
                             }else{
