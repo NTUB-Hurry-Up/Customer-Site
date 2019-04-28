ac6b914c4cbc8baf26f3aa96457743d0b934e8cc
joe890120 <joelin890120@gmail.com>
2019-04-28 10:01:22 +0800

    linebot108508

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var storeList = function (){
             event.reply('執行錯誤');
         } else {
             console.log("fuction ff");
-            arrStoreList = [];
+            var arrStoreList = [];
             // var o = temp.temp_store_contents
             arrStoreList.push(temp.temp_store);
             // console.log("first-> arr: " + arr.length)
@@ -158,6 +158,9 @@ var storeList = function (){
 
             }
             console.log(arrStoreList[0])
+            return arrStoreList[0];
+            arrStoreList[0].contents.contents.length = 0;
+            arrStoreList.length = 0;
             data.length = 0;
         }
     })
@@ -192,7 +195,6 @@ var status = "";
 var statusTime = 0;
 var arrCart = [];
 var arrCartQty = [];
-var arrStoreList = [];
 bot.on('message', function (event) {
     event.source.profile().then(
         function (profile) {
@@ -242,11 +244,9 @@ bot.on('message', function (event) {
                 }
             }else if(msg1 == "店家") {
                 if(msg2 == "資訊") {
-                    storeList()
-                    console.log("arrStoreList--->"+arrStoreList[0]);
-                    event.reply(arrStoreList[0]);
-                    // arrStoreList[0].contents.contents.length = 0;
-                    // arrStoreList.length = 0;
+                    var x = storeList()
+                    consloe.log("x->"+x)
+                    event.reply(x);
                     // storeList();                
                 }else if(msg2 == "查看菜單") {
                     store.fetchStorefood(msg3).then(data => {
