3a544f5c92ade08cf2fb1d8ff8d6d74dd5cb4f99
joe890120 <joelin890120@gmail.com>
2019-04-28 13:50:30 +0800

    linebot108508

--- temp.js
+++ temp.js
@@ -154,6 +154,17 @@ var temp_cart={
           "color": "#58290C"
         },
         {
+          "type": "buttons",
+          "actions": [
+              {
+                "type": "datetimepicker",
+                "label": "時間日期",
+                "mode": "datetime",
+                "data": "datetime"
+              }
+          ]
+        },
+        {
           "type": "button",
           "action": {
             "type": "message",
