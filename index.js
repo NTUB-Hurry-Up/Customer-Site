138deb50295ed8e242d5bedae42c20a6c955cf90
joe890120 <joelin890120@gmail.com>
2019-04-28 15:38:39 +0800

    linebot108508

--- index.js
+++ index.js
@@ -614,7 +614,7 @@ bot.on('message', function (event) {
                         }
                         if(arrCart[0].length>3){
                             template.contents.body.contents[6].contents[0].text = "取餐時間 : "+arrCart[0][3]+" "+arrCart[0][4];
-                            template.contents.footer.contents[2].action.lable="修改取餐時間"
+                            template.contents.footer.contents[2].action.label="修改取餐時間"
                         }else{
                             template.contents.body.contents[6].contents[0].text = "取餐時間 : 未輸入";
                         }
