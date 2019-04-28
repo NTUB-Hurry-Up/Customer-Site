984fc0896cca40a3d32d40f7294e341f52944971
joe890120 <joelin890120@gmail.com>
2019-04-28 13:32:34 +0800

    linebot108508

--- index.js
+++ index.js
@@ -644,10 +644,12 @@ bot.on('postback', function (event) {
             if(data === "datetime"){
                 data += `${JSON.stringify(event.postback.params)}`;
                 console.log(`${JSON.stringify(event.postback.params)}`)
-                var NewArray = data.split(":");
-                for(var i = 0; i<NewArray.length; i++){
-                    console.log("arr+"+NewArray[i])
-                }
+                var NewArray = data.substr(13,10)
+                // for(var i = 0; i<NewArray.length; i++){
+                //     console.log("arr+"+NewArray[i])
+                // }
+                console.log(NewArray)
+
                 event.reply(`Got postback: ${data}`);
             }
         }
