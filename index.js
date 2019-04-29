11497d3acca56d2004b0e4bda5fbb6482f2a6a97
joe890120 <joelin890120@gmail.com>
2019-04-29 17:09:01 +0800

    linebot108508

--- index.js
+++ index.js
@@ -612,24 +612,24 @@ bot.on('message', function (event) {
                         }
                         console.log(temp.temp_cart.contents.footer.contents[2].action[0])
 
-                        // var today=new Date();
-                        // //--date-time-formate---start------
-                        // var cMINMonth=(today.getMonth()+1<10 ? '0' : '')+(today.getMonth()+1)
-                        // var cMAXMonth=(today.getMonth()+3<10 ? '0' : '')+(today.getMonth()+3)
-                        // var cDay=(today.getDate()<10 ? '0' : '')+today.getDate();
+                        var today=new Date();
+                        //--date-time-formate---start------
+                        var cMINMonth=(today.getMonth()+1<10 ? '0' : '')+(today.getMonth()+1)
+                        var cMAXMonth=(today.getMonth()+3<10 ? '0' : '')+(today.getMonth()+3)
+                        var cDay=(today.getDate()<10 ? '0' : '')+today.getDate();
 
-                        // var cHours = (today.getHours()+8 < 10 ? '0' : '')+(today.getHours()+8);
-                        // var cMinutes = (today.getMinutes()<10 ? '0' : '')+today.getMinutes();
-                        // //--date-time-formate---end--------
-                        // var cOrderMIN =today.getFullYear()+"-"+cMINMonth+"-"+cDay+"t"+cHours+':'+cMinutes;
-                        // var cOrderMAX =today.getFullYear()+"-"+cMAXMonth+"-"+cDay+"t"+cHours+':'+cMinutes;
-                        // // var cOrderTime =cHours+':'+cMinutes;
+                        var cHours = (today.getHours()+8 < 10 ? '0' : '')+(today.getHours()+8);
+                        var cMinutes = (today.getMinutes()<10 ? '0' : '')+today.getMinutes();
+                        //--date-time-formate---end--------
+                        var cOrderMIN =today.getFullYear()+"-"+cMINMonth+"-"+cDay+"T"+cHours+':'+cMinutes;
+                        var cOrderMAX =today.getFullYear()+"-"+cMAXMonth+"-"+cDay+"T"+cHours+':'+cMinutes;
+                        // var cOrderTime =cHours+':'+cMinutes;
 
-                        // console.log(cOrderMIN);
-                        // console.log(cOrderMAX);
-                        // cOrderMIN.toString();
-                        // template.contents.footer.contents[2].action.min = cOrderMIN
-                        // template.contents.footer.contents[2].action.max = cOrderMAXDate+"t"+cOrderTime
+                        console.log(cOrderMIN);
+                        console.log(cOrderMAX);
+                        cOrderMIN.toString();
+                        template.contents.footer.contents[2].action.min = cOrderMIN
+                        template.contents.footer.contents[2].action.max = cOrderMAXDate+"t"+cOrderTime
 
                         template.contents.body.contents[7].contents[0].text = "總價 : $"+cartTotalPrice;
                         template.contents.footer.contents[0].action.text="購物車,清空"//清空購物車
