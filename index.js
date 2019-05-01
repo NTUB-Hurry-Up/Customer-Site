3fb9fd1dd9ffac667cc85185718ecbc43f8cf605
_Joe <joelin890120@gmail.com>
2019-05-02 01:14:13 +0800

    linebot108508

--- index.js
+++ index.js
@@ -639,8 +639,8 @@ bot.on('message', function (event) {
                         console.log(cOrderMIN);
                         console.log(cOrderMAX);
                         cOrderMIN.toString();
-                        template.contents.footer.contents[2].action.min = cOrderMIN
-                        template.contents.footer.contents[2].action.max = cOrderMAX
+                        // template.contents.footer.contents[2].action.min = cOrderMIN
+                        // template.contents.footer.contents[2].action.max = cOrderMAX
 
                         template.contents.body.contents[7].contents[0].text = "總價 : $"+cartTotalPrice;
                         template.contents.footer.contents[0].action.text="購物車,清空"//清空購物車
