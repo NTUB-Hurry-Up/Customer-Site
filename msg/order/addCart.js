3690a9e0d1a46c9d6cce094965c30723df74df5b
joe890120 <joelin890120@gmail.com>
2019-05-04 21:43:32 +0800

    linebot108508

--- msg/order/addCart.js
+++ msg/order/addCart.js
@@ -25,7 +25,7 @@ var addCart = function(event, CartA, CartQ, cstoreid, cfoodid, cstoreName, cfood
                         // CartA = index.objCart.arrCart.length
                         CartA = index.objCart
                         console.log(CartA)
-                        console.log("CartA->"+CartA+", objCart.arrCart.length->"+index.objCart.arrCart.length)
+                        // console.log("CartA->"+CartA+", objCart.arrCart.length->"+index.objCart.arrCart.length)
                     }
                     index.objCart.arrCart[CartA]={
                         'userid' : userId,
@@ -34,41 +34,41 @@ var addCart = function(event, CartA, CartQ, cstoreid, cfoodid, cstoreName, cfood
                         'arrfood' : []
                     }
                 }
-                console.log("objCart.arrCart[CartA].storeid-->"+index.objCart.arrCart[CartA].storeid)
-                console.log("cstoreid-->"+cstoreid)
-                if(index.objCart.arrCart[CartA].storeid == cstoreid){
-                    // arrCartQty=[cfoodid, cfoodName, cfoodPrice];
-                    if(CartQ == -1){
-                        CartQ = index.objCartQty.arrQty.length
-                    }
-                    index.objCartQty.arrQty[CartQ]={
-                        'userid' : userId,
-                        'foodid' : cfoodid, 
-                        'foodName' : cfoodName, 
-                        'foodPrice' : cfoodPrice
-                    }
-                    console.log(index.objCartQty.arrQty[CartQ])
+                // console.log("objCart.arrCart[CartA].storeid-->"+index.objCart.arrCart[CartA].storeid)
+                // console.log("cstoreid-->"+cstoreid)
+                // if(index.objCart.arrCart[CartA].storeid == cstoreid){
+                //     // arrCartQty=[cfoodid, cfoodName, cfoodPrice];
+                //     if(CartQ == -1){
+                //         CartQ = index.objCartQty.arrQty.length
+                //     }
+                //     index.objCartQty.arrQty[CartQ]={
+                //         'userid' : userId,
+                //         'foodid' : cfoodid, 
+                //         'foodName' : cfoodName, 
+                //         'foodPrice' : cfoodPrice
+                //     }
+                //     console.log(index.objCartQty.arrQty[CartQ])
 
                     
                     
-                    // status = "arrCartQty";
-                    // statusTime = 2;
-                    event.reply("數量 ?");
-                }else{
-                    const template = temp.temp1.template;
-                    template.actions[0].type = "message";
-                    template.actions[0].label = "是";
-                    template.actions[0].text = msg1+",查看菜單,"+cstoreid+",是";
+                //     // status = "arrCartQty";
+                //     // statusTime = 2;
+                //     event.reply("數量 ?");
+                // }else{
+                //     const template = temp.temp1.template;
+                //     template.actions[0].type = "message";
+                //     template.actions[0].label = "是";
+                //     template.actions[0].text = msg1+",查看菜單,"+cstoreid+",是";
 
-                    template.actions[1].type = "message";
-                    template.actions[1].label = "否";
-                    template.actions[1].text = msg1+",查看菜單,"+index.objCart.arrCart[CartA].storeid+",否";
-                    template.title = "購物車訊息"
-                    template.text = "要改下訂這家店嗎 ?"
-                    // status = "changeStore";
-                    event.reply(temp.temp1);
+                //     template.actions[1].type = "message";
+                //     template.actions[1].label = "否";
+                //     template.actions[1].text = msg1+",查看菜單,"+index.objCart.arrCart[CartA].storeid+",否";
+                //     template.title = "購物車訊息"
+                //     template.text = "要改下訂這家店嗎 ?"
+                //     // status = "changeStore";
+                //     event.reply(temp.temp1);
                     
-                }
+                // }
 
             }
         })
