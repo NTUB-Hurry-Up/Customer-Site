2beb7adda103c417756dc1b0eb5fe81a9542a3e1
joe890120 <joelin890120@gmail.com>
2019-05-04 16:55:59 +0800

    linebot108508

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const temp = require('./temp');
 const order = require('./order');
 const memInfo = require('./msg/member/memInfo');
 const storeInfo = require('./msg/store/storeInfo');
+const foodInfo = require('./msg/foodInfo/foodInfo');
 //----------------------------------------
 // 填入自己在Line Developers的channel值
 //----------------------------------------
@@ -88,110 +89,7 @@ bot.on('message', function (event) {
                 if(msg2 == "資訊") {
                     storeInfo.storeInfo(event)
                 }else if(msg2 == "查看菜單") {
-                    store.fetchStorefood(msg3).then(data => {
-                        if (data == -1) {
-                            event.reply('找不到資料');
-                        } else if (data == -9) {
-                            event.reply('執行錯誤');
-                        } else {
-                            var arr2 = [];
-                            arr2.push(temp.temp_menu);
-                            for (var i = 0; i < data.length; i++) {
-                                console.log(data[i].foodid + " " + data[i].foodPrice + " " + data[i].foodName)
-                                arr2[0].contents.contents.push({
-
-                                    "type": "bubble",
-                                    "hero": {
-                                        "type": "image",
-                                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-                                        "size": "full",
-                                        "aspectRatio": "20:13",
-                                        "aspectMode": "cover",
-                                        "action": {
-                                            "type": "uri",
-                                            "label": "Action",
-                                            "uri": "https://linecorp.com"
-                                        }
-                                    },
-                                    "body": {
-                                        "type": "box",
-                                        "layout": "vertical",
-                                        "spacing": "md",
-                                        "action": {
-                                            "type": "uri",
-                                            "label": "Action",
-                                            "uri": "https://linecorp.com"
-                                        },
-                                        "contents": [
-                                            {
-                                                "type": "text",
-                                                "text": data[i].foodName,
-                                                "size": "xl",
-                                                "weight": "bold"
-                                            },
-                                            {
-                                                "type": "box",
-                                                "layout": "vertical",
-                                                "spacing": "sm",
-                                                "contents": [
-                                                    {
-                                                        "type": "box",
-                                                        "layout": "baseline",
-                                                        "contents": [
-                                                            {
-                                                                "type": "text",
-                                                                "text": "Price",
-                                                                "flex": 1,
-                                                                "size": "lg",
-                                                                "color": "#AAAAAA"
-                                                            },
-                                                            {
-                                                                "type": "text",
-                                                                "text": "NT$"+data[i].foodPrice,
-                                                                "flex": 0,
-                                                                "margin": "lg",
-                                                                "size": "lg",
-                                                                "align": "end",
-                                                                "weight": "regular"
-                                                            }
-                                                        ]
-                                                    }
-                                                ]
-                                            },
-                                            {
-                                                "type": "text",
-                                                "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
-                                                "size": "xs",
-                                                "color": "#AAAAAA",
-                                                "wrap": true
-                                            }
-                                        ]
-                                    },
-                                    "footer": {
-                                        "type": "box",
-                                        "layout": "vertical",
-                                        "contents": [
-                                            {
-                                                "type": "button",
-                                                "action": {
-                                                    "type": "message",
-                                                    "label": "Add to Cart",
-                                                    "text": "店家,加入購物車,"+data[i].storeid+","+data[i].foodid
-                                                },
-                                                "color": "#905C44",
-                                                "style": "primary"
-                                            }
-                                        ]
-                                    }
-
-                                })
-                            }
-                            event.reply(arr2[0]);
-                            arr2[0].contents.contents.length = 0;
-                            arr2.length = 0;
-                            data.length = 0;
-                        }
-                    })
+                    foodInfo.foodInfo(event, msg3)
                 }else if(msg2 == "聯絡店家") {
                     store.fetchStoreTel(msg3).then(data => {
                         if (data == -1) {
--- msg/member/memInfo.js
+++ msg/member/memInfo.js
@@ -7,33 +7,27 @@ const member = require('./../../member');
 //------------------------------------------
 var memInfo = function(event){
     //存放結果
-        event.source.profile().then(function (profile) {
-            member.fetchMember(profile.userId).then(data => {
-                if (data == -1) {
-                    console.log("找不到資料")
-                } else if (data == -9) {
-                    console.log("執行錯誤")
-                } else {
-                    const template = temp.temp1.template;
-                    template.actions[0].type = "message";
-                    template.actions[0].label = "修改姓名";
-                    template.actions[0].text = "會員,修改姓名";
-        
-                    template.actions[1].type = "message";
-                    template.actions[1].label = "修改電話";
-                    template.actions[1].text = "會員,修改電話";
-                    template.title = "會員資訊"
-                    template.text = "姓名 : " + data.name + "\n電話 : " + data.phone
-                    event.reply( temp.temp1);
-                }
-            })
-          });
-        
-    //新增會員資料
+    event.source.profile().then(function (profile) {
+        member.fetchMember(profile.userId).then(data => {
+            if (data == -1) {
+                console.log("找不到資料")
+            } else if (data == -9) {
+                console.log("執行錯誤")
+            } else {
+                const template = temp.temp1.template;
+                template.actions[0].type = "message";
+                template.actions[0].label = "修改姓名";
+                template.actions[0].text = "會員,修改姓名";
     
-   
-
-    //回傳執行結果
+                template.actions[1].type = "message";
+                template.actions[1].label = "修改電話";
+                template.actions[1].text = "會員,修改電話";
+                template.title = "會員資訊"
+                template.text = "姓名 : " + data.name + "\n電話 : " + data.phone
+                event.reply( temp.temp1);
+            }
+        })
+    });
 }
 
 
