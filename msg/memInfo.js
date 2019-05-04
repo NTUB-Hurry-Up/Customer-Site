0a17acababfdea8f42df3e3e3714fdc9146abca1
joe890120 <joelin890120@gmail.com>
2019-05-04 10:33:54 +0800

    linebot108508

--- msg/memInfo.js
+++ msg/memInfo.js
@@ -9,7 +9,27 @@ const member = require('./../member');
 //------------------------------------------
 var memInfo = function(id){
     //存放結果
-    var result={};  
+    var result={
+        "type": "template",
+        "altText": "this is a buttons template",
+        "template": {
+            "type": "buttons",
+            "actions": [
+                {
+                "type": "message",
+                "label": "修改姓名",
+                "text": "會員,修改姓名"
+                },
+                {
+                "type": "message",
+                "label": "修改電話",
+                "text": "會員,修改電話"
+                }
+            ],
+            "title": "會員資訊",
+            "text": "顆顆"
+        }
+    } 
 
     //新增會員資料
     member.fetchMember(id).then(data => {
@@ -18,32 +38,9 @@ var memInfo = function(id){
         } else if (data == -9) {
             event.reply('執行錯誤');
         } else {
-            result={
-                "type": "template",
-                "altText": "this is a buttons template",
-                "template": {
-                    "type": "buttons",
-                    "actions": [
-                        {
-                        "type": "message",
-                        "label": "修改姓名",
-                        "text": "會員,修改姓名"
-                        },
-                        {
-                        "type": "message",
-                        "label": "修改電話",
-                        "text": "會員,修改電話"
-                        }
-                    ],
-                    "title": "會員資訊",
-                    "text": "姓名 : " + data.name + "\n電話 : " + data.phone
-                }
-            }
-            console.log(result)
+            template.text="姓名 : " + data.name + "\n電話 : " + data.phone;
         }
     })
-    setTimeout(function(){ console.log(result)},10000)
-   
     return result; 
 
     //回傳執行結果
