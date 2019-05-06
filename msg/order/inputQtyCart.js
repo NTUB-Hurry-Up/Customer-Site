6444d352752fb89644ed78f9bc2fb7ccc1429fa7
joe890120 <joelin890120@gmail.com>
2019-05-06 14:41:57 +0800

    linebot108508

--- msg/order/inputQtyCart.js
+++ msg/order/inputQtyCart.js
@@ -96,9 +96,9 @@ var inputQtyCart = function(event, objCart, CartA, userName){
         // template.contents.footer.contents[2].action.min = cOrderMIN
         // template.contents.footer.contents[2].action.max = cOrderMAX
 
-        template.contents.body.contents[7].contents[0].text = "總價 : $"+cartTotalPrice;
+        template.contents.body.contents[7].contents[0].text = "總價 : $"//+cartTotalPrice;
         template.contents.footer.contents[0].action.text="購物車,清空"//清空購物車
-        template.contents.footer.contents[1].action.text="店家,查看菜單,"+objCart.arrCart[CartA].storeid;//繼續點餐
+        template.contents.footer.contents[1].action.text="店家,查看菜單,"//+objCart.arrCart[CartA].storeid;//繼續點餐
         
         //console.log("total "+cartTotalPrice);
         event.reply(template);
