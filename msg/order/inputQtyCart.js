fac51dd4644e1cd618079df3d08c7e56bd927c36
joe890120 <joelin890120@gmail.com>
2019-05-06 14:40:43 +0800

    linebot108508

--- msg/order/inputQtyCart.js
+++ msg/order/inputQtyCart.js
@@ -15,7 +15,7 @@ var inputQtyCart = function(event, objCart, CartA, userName){
         template.contents.body.contents[1].contents[0].text = cstoreName;
         var arr=[];
         arr.push(template)
-        arr[0].contents.body.contents[4].contents.length=0
+        // arr[0].contents.body.contents[4].contents.length=0
         
         // var cartTotalPrice = 0;
         // var Afood=objCart.arrCart[CartA].arrfood[k]
@@ -63,8 +63,8 @@ var inputQtyCart = function(event, objCart, CartA, userName){
         //     template.contents.footer.contents[2].action.label = "修改取餐時間"
             
         // }else{
-            template.contents.body.contents[6].contents[0].text = "取餐時間 : 未輸入";
-            template.contents.footer.contents[2].action.label = "輸入取餐時間"
+            // template.contents.body.contents[6].contents[0].text = "取餐時間 : 未輸入";
+            // template.contents.footer.contents[2].action.label = "輸入取餐時間"
         // }
         // //console.log(temp.temp_cart.contents.footer.contents[2].action[0])
 
@@ -96,9 +96,9 @@ var inputQtyCart = function(event, objCart, CartA, userName){
         // template.contents.footer.contents[2].action.min = cOrderMIN
         // template.contents.footer.contents[2].action.max = cOrderMAX
 
-        // template.contents.body.contents[7].contents[0].text = "總價 : $"+cartTotalPrice;
-        // template.contents.footer.contents[0].action.text="購物車,清空"//清空購物車
-        // template.contents.footer.contents[1].action.text="店家,查看菜單,"+objCart.arrCart[CartA].storeid;//繼續點餐
+        template.contents.body.contents[7].contents[0].text = "總價 : $"+cartTotalPrice;
+        template.contents.footer.contents[0].action.text="購物車,清空"//清空購物車
+        template.contents.footer.contents[1].action.text="店家,查看菜單,"+objCart.arrCart[CartA].storeid;//繼續點餐
         
         //console.log("total "+cartTotalPrice);
         event.reply(template);
