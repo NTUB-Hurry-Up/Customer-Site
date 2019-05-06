20b2cfed4ff102c37ed5a350affa990fe3c212eb
joe890120 <joelin890120@gmail.com>
2019-05-06 15:40:38 +0800

    linebot108508

--- msg/order/Cart.js
+++ msg/order/Cart.js
@@ -6,7 +6,7 @@ const temp = require('./../../temp');
 //------------------------------------------
 var Cart = function(event, oCart, userName){
     event.source.profile().then(function (profile) {
-        // var i = objCart.arrCart[CartA].arrfood.length;
+        // var i = oCart.arrfood.length;
         var cstoreid = oCart.storeid;
         var cstoreName = oCart.storeName;
 
@@ -15,58 +15,58 @@ var Cart = function(event, oCart, userName){
         template.contents.body.contents[1].contents[0].text = cstoreName;
         var arr=[];
         arr.push(template)
-        // arr[0].contents.body.contents[4].contents.length=0
+        arr[0].contents.body.contents[4].contents.length=0
         
         var cartTotalPrice = 0;
-        // var Afood=objCart.arrCart[CartA].arrfood[k]
-        // console.log("CARTCART-->"+objCart.arrCart[CartA].arrfood)
-        // for(var k = 1; k<i; k++){
-        //     cartTotalPrice += Afood.foodPrice*Afood.foodQty
-        //     //console.log("i="+i+" ,k="+k)
+        console.log("CARTCART-->"+oCart.arrfood)
+        for(var k = 1; k<i; k++){
+            var Afood=oCart.arrfood[k]
+            cartTotalPrice += Afood.foodPrice*Afood.foodQty
+            //console.log("i="+i+" ,k="+k)
 
-        //     //console.log(arrCart)
-        //     //console.log(arrCart[k][0]+", "+arrCart[k][1])
-        //     arr[0].contents.body.contents[4].contents.push(
-        //         {
-        //             "type": "box",
-        //             "layout": "baseline",
-        //             "contents": [
-        //             {
-        //                 "type": "text",
-        //                 "text": Afood.foodName,
-        //                 "flex": 0,
-        //                 "margin": "sm",
-        //                 "size": "md",
-        //                 "weight": "bold"
-        //             },
-        //             {
-        //                 "type": "text",
-        //                 "text": Afood.foodQty,
-        //                 "size": "xs",
-        //                 "align": "center",
-        //                 "color": "#AAAAAA",
-        //                 "wrap": true
-        //             },
-        //             {
-        //                 "type": "text",
-        //                 "text": "$ "+Afood.foodPrice*Afood.foodQty,
-        //                 "size": "sm",
-        //                 "align": "end",
-        //                 "color": "#000000"
-        //             }
-        //             ]
-        //         }
-        //     );
-        // }
-        // if(objCart.arrCart[CartA].takeDate.length != 0){
+            //console.log(arrCart)
+            //console.log(arrCart[k][0]+", "+arrCart[k][1])
+            arr[0].contents.body.contents[4].contents.push(
+                {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": Afood.foodName,
+                        "flex": 0,
+                        "margin": "sm",
+                        "size": "md",
+                        "weight": "bold"
+                    },
+                    {
+                        "type": "text",
+                        "text": Afood.foodQty,
+                        "size": "xs",
+                        "align": "center",
+                        "color": "#AAAAAA",
+                        "wrap": true
+                    },
+                    {
+                        "type": "text",
+                        "text": "$ "+Afood.foodPrice*Afood.foodQty,
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#000000"
+                    }
+                    ]
+                }
+            );
+        }
+        if(oCart.takeDate.length != 0){
 
-        //     template.contents.body.contents[6].contents[0].text = "取餐時間 : "+objCart.arrCart[CartA].takeDate+" "+objCart.arrCart[CartA].takeTime;
-        //     template.contents.footer.contents[2].action.label = "修改取餐時間"
+            template.contents.body.contents[6].contents[0].text = "取餐時間 : "+oCart.takeDate+" "+oCart.takeTime;
+            template.contents.footer.contents[2].action.label = "修改取餐時間"
             
-        // }else{
-        //     template.contents.body.contents[6].contents[0].text = "取餐時間 : 未輸入";
-        //     template.contents.footer.contents[2].action.label = "輸入取餐時間"
-        // }
+        }else{
+            template.contents.body.contents[6].contents[0].text = "取餐時間 : 未輸入";
+            template.contents.footer.contents[2].action.label = "輸入取餐時間"
+        }
         // //console.log(temp.temp_cart.contents.footer.contents[2].action[0])
 
         // var today=new Date();
@@ -99,7 +99,7 @@ var Cart = function(event, oCart, userName){
 
         template.contents.body.contents[7].contents[0].text = "總價 : $"+cartTotalPrice;
         template.contents.footer.contents[0].action.text="購物車,清空"//清空購物車
-        template.contents.footer.contents[1].action.text="店家,查看菜單,"//+objCart.arrCart[CartA].storeid;//繼續點餐
+        template.contents.footer.contents[1].action.text="店家,查看菜單,"+oCart.storeid;//繼續點餐
         
         //console.log("total "+cartTotalPrice);
         event.reply(template);
