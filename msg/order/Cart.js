0e56b26a2f309800db596d06b5f1ec90583d4de2
joe890120 <joelin890120@gmail.com>
2019-05-06 15:53:01 +0800

    linebot108508

--- msg/order/Cart.js
+++ msg/order/Cart.js
@@ -15,7 +15,7 @@ var Cart = function(event, oCart, userName){
         template.contents.body.contents[1].contents[0].text = cstoreName;
         var arr=[];
         arr.push(template)
-        arr[0].contents.body.contents[4].contents.length=0
+        // arr[0].contents.body.contents[4].contents.length=0
         
         var cartTotalPrice = 0;
         console.log("CARTCART-->"+oCart.arrfood)
@@ -102,7 +102,7 @@ var Cart = function(event, oCart, userName){
         template.contents.footer.contents[1].action.text="店家,查看菜單,"+oCart.storeid;//繼續點餐
         
         //console.log("total "+cartTotalPrice);
-        event.reply(template);
+        event.reply(arr);
     });
 }
 
