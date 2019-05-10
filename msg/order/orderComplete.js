ce16904ca061eb8fecd7fe237f1ba083d2ce1422
joe890120 <joelin890120@gmail.com>
2019-05-10 20:30:29 +0800

    linebot108508

--- msg/order/orderComplete.js
+++ msg/order/orderComplete.js
@@ -15,20 +15,69 @@ var orderComplete = function (event, oCart, cOrderid) {
         var arr = [];
         arr.push(lodash.cloneDeep(temp.orderComplete))
         for (var k = 0; k < i; k++) {
-            var tempRe = lodash.cloneDeep(temp.orderCompleteRepeat)
+            // var tempRe = lodash.cloneDeep(temp.orderCompleteRepeat)
             var Afood=oCart.arrfood[k]
             var cfoodid = Afood.foodid;
             var cfoodName = Afood.foodName;
             var cfoodPrice = Afood.foodPrice;
             var cfoodQty = Afood.foodQty;
             var foodAmt = cfoodPrice * cfoodQty;
-            tempRe.contents[0].contents[0].text=cfoodName
-            tempRe.contents[1].contents[0].text=cfoodQty
-            tempRe.contents[2].contents[0].text=cfoodPrice
-            arr[0].contents.body.contents[5].contents[2].push(tempRe)
+            // tempRe.contents[0].contents[0].text=cfoodName
+            // tempRe.contents[1].contents[0].text=cfoodQty
+            // tempRe.contents[2].contents[0].text=cfoodPrice
+            arr[0].contents.body.contents[5].contents[2].push({
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "xxl",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": cfoodName,
+                        "flex": 0,
+                        "size": "sm",
+                        "align": "start",
+                        "weight": "bold",
+                        "wrap": true
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": cfoodQty,
+                        "size": "md",
+                        "align": "center",
+                        "color": "#AAAAAA"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "$"+cfoodPrice,
+                        "margin": "xxl",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#AAAAAA",
+                        "wrap": false
+                      }
+                    ]
+                  }
+                ]
+              })
             console.log(cfoodid+", "+cfoodPrice+", "+cfoodQty+", "+foodAmt)
         }
-        event.reply(arr[0]);
+        event.reply(arr);
         
     });
 }
