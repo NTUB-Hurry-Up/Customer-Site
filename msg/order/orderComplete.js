68609e0f44523e290122f88a4aa3cb36cf2da233
joe890120 <joelin890120@gmail.com>
2019-05-10 19:57:11 +0800

    linebot108508

--- msg/order/orderComplete.js
+++ msg/order/orderComplete.js
@@ -7,9 +7,18 @@ const sendOrder = require('./sendOrder');
 //------------------------------------------
 // 查詢所有的店家
 //------------------------------------------
-var orderComplete = function (event, oCart) {
+var orderComplete = function (event, oCart, cOrderid) {
     event.source.profile().then(function (profile) {
-        
+        console.log(cOrderid)
+        var i = oCart.arrfood.length;
+        for (var k = 0; k < i; k++) {
+            var Afood=oCart.arrfood[k]
+            var cfoodid = Afood.foodid;
+            var cfoodPrice = Afood.foodPrice;
+            var cfoodQty = Afood.foodQty;
+            var foodAmt = cfoodPrice * cfoodQty;
+            console.log(cfoodid+", "+cfoodPrice+", "+cfoodQty+", "+foodAmt)
+        }
     });
 }
 
