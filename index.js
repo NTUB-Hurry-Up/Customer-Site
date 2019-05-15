f7bb3a842df226a6e8e43d14aaef236ca6af22bb
joe890120 <joelin890120@gmail.com>
2019-05-15 18:35:11 +0800

    linebot108508

--- index.js
+++ index.js
@@ -275,11 +275,11 @@ bot.on('message', function (event) {
                                 arr[0].contents.contents[scnt].body.contents[0].text = data[i].orderid
                                 arr[0].contents.contents[scnt].body.contents[1].contents[1].text = data[i].storeName
                                 arr[0].contents.contents[scnt].body.contents[2].contents[1].text = data[i].orderid
-                                arr[0].contents.contents[scnt].body.contents[3].contents[1].text = data[i].status
-                                // arr[0].contents.contents[scnt].body.contents[4].contents[1].text = data[i].orderDate
-                                // arr[0].contents.contents[scnt].body.contents[4].contents[2].text = data[i].orderTime
-                                // arr[0].contents.contents[scnt].body.contents[5].contents[1].text = data[i].takeDate
-                                // arr[0].contents.contents[scnt].body.contents[5].contents[2].text = data[i].takeTime
+                                // arr[0].contents.contents[scnt].body.contents[3].contents[1].text = data[i].status
+                                arr[0].contents.contents[scnt].body.contents[4].contents[1].text = data[i].orderDate
+                                arr[0].contents.contents[scnt].body.contents[4].contents[2].text = data[i].orderTime
+                                arr[0].contents.contents[scnt].body.contents[5].contents[1].text = data[i].takeDate
+                                arr[0].contents.contents[scnt].body.contents[5].contents[2].text = data[i].takeTime
                             }
                             var tempRe = lodash.cloneDeep(temp.orderCompleteRepeat)
                             arr[0].contents.contents[scnt].body.contents[6].contents[fcnt+2]=tempRe
