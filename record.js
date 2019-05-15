b457072b21a559b1e1e58e0c7ad9818d80ceeee2
joe890120 <joelin890120@gmail.com>
2019-05-15 20:05:02 +0800

    linebot108508

--- record.js
+++ record.js
@@ -49,7 +49,7 @@ var fetchOrder = async function(userid){
     let result;  
 
     //讀取資料庫
-    await query('select a.orderid, c.storeid,d."storeName", c."orderDate", c."orderTime", c."takeDate", c."takeTime", c.status, b."foodName", a.quantity, a."unitPrice", a.amount from "orderDetail" a, food b , "order" c , store d where a.foodid = b.foodid and c.orderid in (select orderid from "order" where userid = $1 LIMIT 10) and c.orderid = a.orderid and c.storeid = d.storeid ORDER BY "orderDate"desc, "orderTime" ', [userid])
+    await query('select a.orderid, c.storeid,d."storeName",d."storeAdd", c."orderDate", c."orderTime", c."takeDate", c."takeTime", c.status, b."foodName", a.quantity, a."unitPrice", a.amount from "orderDetail" a, food b , "order" c , store d where a.foodid = b.foodid and c.orderid in (select orderid from "order" where userid = $1 LIMIT 10) and c.orderid = a.orderid and c.storeid = d.storeid ORDER BY "orderDate"desc, "orderTime" ', [userid])
         .then((data) => {
             if(data.rows.length > 0){
                 result = data.rows;  //學生資料(物件)
