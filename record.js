c37d7fe26815e6469f7a7bbf367721165ebc34b6
joe890120 <joelin890120@gmail.com>
2019-05-20 19:59:27 +0800

    linebot108508

--- record.js
+++ record.js
@@ -2,47 +2,6 @@
 
 //引用操作資料庫的物件
 const query = require('./asyncDB');
-
-
-var fetchOrderMaster = async function(userid){
-    //存放結果
-    let result;  
-
-    //讀取資料庫
-    await query('select * from "order" where userid = $1 ORDER BY "orderDate" desc, "orderTime" desc LIMIT 10', [userid])
-        .then((data) => {
-            if(data.rows.length > 0){
-                result = data.rows;  //學生資料(物件)
-            }else{
-                result = -1;  //找不到資料
-            }    
-        }, (error) => {
-            result = -9;  //執行錯誤
-        });
-
-    //回傳執行結果
-    return result;
-}
-//---------------------------------------------------------
-var fetchOrderDetail = async function(orderid){
-    //存放結果
-    let result;  
-
-    //讀取資料庫
-    await query('select a.orderid, b."foodName", a.quantity, a."unitPrice", a.amount from "orderDetail" a, food b where a.foodid = b.foodid and orderid = $1', [orderid])
-        .then((data) => {
-            if(data.rows.length > 0){
-                result = data.rows;  //學生資料(物件)
-            }else{
-                result = -1;  //找不到資料
-            }    
-        }, (error) => {
-            result = -9;  //執行錯誤
-        });
-
-    //回傳執行結果
-    return result;
-}
 //---------------------------------------------------------
 var fetchOrder = async function(userid){
     //存放結果
@@ -64,5 +23,5 @@ var fetchOrder = async function(userid){
     return result;
 }
 //匯出
-module.exports = {fetchOrderMaster, fetchOrderDetail, fetchOrder};
+module.exports = {fetchOrder};
 
