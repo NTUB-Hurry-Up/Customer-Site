df7c90dda0dbb28ebb0cbb3b96be8411df43b169
_Joe <joelin890120@gmail.com>
2019-05-27 00:02:45 +0800

    linebot108508

--- msg/store/storeInfo.js
+++ msg/store/storeInfo.js
@@ -5,7 +5,7 @@ const store = require('./../../store');
 //------------------------------------------
 // 查詢所有的店家
 //------------------------------------------
-var storeInfo = function(event, lodash){
+var storeInfo = function (event, lodash) {
     event.source.profile().then(function (profile) {
         store.fetchStore().then(data => {
             if (data == -1) {
@@ -14,7 +14,7 @@ var storeInfo = function(event, lodash){
                 event.reply('執行錯誤');
             } else {
                 var arrStoreList = [];
-                arrStoreList.push(temp.temp_store);
+                arrStoreList.push(lodash.cloneDeep(temp.temp_store));
                 for (var i = 0; i < data.length; i++) {
                     // (function(o){
                     //     o.body.contents[0].text=data[i].storeName;
@@ -22,125 +22,23 @@ var storeInfo = function(event, lodash){
                     //     o.body.contents[1].contents[1].contents[1].text=data[i].storeTel;
                     //     arr[0].contents.contents.push(o);
                     // })(Object.assign({}, o));
-                    arrStoreList[0].contents.contents.push(
-                        {
-                            "type": "bubble",
-                            "hero": {
-                                "type": "image",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                                "size": "full",
-                                "aspectRatio": "20:13",
-                                "aspectMode": "cover"
-                            },
-                            "body": {
-                                "type": "box",
-                                "layout": "vertical",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "text": data[i].storeName,
-                                        "size": "xl",
-                                        "weight": "bold"
-                                    },
-                                    {
-                                        "type": "box",
-                                        "layout": "vertical",
-                                        "spacing": "sm",
-                                        "margin": "lg",
-                                        "contents": [
-                                            {
-                                                "type": "box",
-                                                "layout": "baseline",
-                                                "spacing": "sm",
-                                                "contents": [
-                                                    {
-                                                        "type": "text",
-                                                        "text": "Place",
-                                                        "flex": 1,
-                                                        "size": "sm",
-                                                        "color": "#AAAAAA"
-                                                    },
-                                                    {
-                                                        "type": "text",
-                                                        "text": data[i].storeAdd,
-                                                        "flex": 5,
-                                                        "size": "sm",
-                                                        "color": "#666666",
-                                                        "wrap": true
-                                                    }
-                                                ]
-                                            },
-                                            {
-                                                "type": "box",
-                                                "layout": "baseline",
-                                                "spacing": "sm",
-                                                "contents": [
-                                                    {
-                                                        "type": "text",
-                                                        "text": "Tel",
-                                                        "flex": 1,
-                                                        "size": "sm",
-                                                        "color": "#AAAAAA"
-                                                    },
-                                                    {
-                                                        "type": "text",
-                                                        "text": data[i].storeTel,
-                                                        "flex": 5,
-                                                        "size": "sm",
-                                                        "color": "#666666",
-                                                        "wrap": true
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
-                            },
-                            "footer": {
-                                "type": "box",
-                                "layout": "vertical",
-                                "flex": 0,
-                                "spacing": "sm",
-                                "contents": [
-                                    {
-                                        "type": "button",
-                                        "action": {
-                                            "type": "message",
-                                            "label": "查看菜單",
-                                            "text": "店家,查看菜單," + data[i].storeid
-                                        },
-                                        "height": "sm",
-                                        "style": "link"
-                                    },
-                                    {
-                                        "type": "button",
-                                        "action": {
-                                            "type": "message",
-                                            "label": "聯絡店家",
-                                            "text": "店家,聯絡店家," + data[i].storeid
-                                        },
-                                        "height": "sm",
-                                        "style": "link"
-                                    },
-                                    {
-                                        "type": "spacer",
-                                        "size": "sm"
-                                    }
-                                ]
-                            }
-                          }
-                    );
+                    arrStoreList[0].contents.contents[i] = lodash.cloneDeep(temp_store_repeat)
 
+                    arrStoreList[0].contents.contents[i].body.contents[0].text = data[i].storeName;
+                    arrStoreList[0].contents.contents[i].body.contents[1].contents[0].contents[1].text = data[i].storeAdd;
+                    arrStoreList[0].contents.contents[i].body.contents[1].contents[1].contents[1].text = data[i].storeTel;
+                    arrStoreList[0].contents.contents[i].footer.contents[0].action.text = "店家,查看菜單," + data[i].storeid;
+                    arrStoreList[0].contents.contents[i].footer.contents[1].action.text = "店家,聯絡店家," + data[i].storeid;
                 }
                 event.reply(arrStoreList[0]);
-                arrStoreList[0].contents.contents.length = 0;
-                arrStoreList.length = 0;
-                data.length = 0;
+                // arrStoreList[0].contents.contents.length = 0;
+                // arrStoreList.length = 0;
+                // data.length = 0;
             }
         })
     });
 }
-var storeTel = function(event, storeid){
+var storeTel = function (event, storeid) {
     event.source.profile().then(function (profile) {
         store.fetchStoreTel(storeid).then(data => {
             if (data == -1) {
@@ -158,7 +56,6 @@ var storeTel = function(event, storeid){
     });
 }
 //匯出
-module.exports = {storeInfo, storeTel};
+module.exports = { storeInfo, storeTel };
 
 
-            
