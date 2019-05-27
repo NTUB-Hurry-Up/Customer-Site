a19aed2850714b64d973ad11d8e846fd67cb44b1
joe890120 <joelin890120@gmail.com>
2019-05-27 14:16:56 +0800

    linebot108508

--- index.js
+++ index.js
@@ -197,57 +197,7 @@ bot.on('message', function (event) {
                     obj2null.status(obj.arrPsnl[objLoc])
                     memInfo.changeMemInfo(event, obj.arrPsnl[objLoc], s, msg, userId)
                 } else if (s == "inputQty") {//work
-                    
-                    var isNum = /^[0-9]+$/;
-                    var x = Boolean(!isNum.test(msg1));
-                    var y = Boolean(parseInt(msg1) < 1);
-                    var z = Boolean(objStatus.arrStatus[Sta].statusTime > 0);
-                    if (z) {
-                        if (x) {
-                            objStatus.arrStatus[Sta].statusTime--;
-                            event.reply([
-                                { 'type': 'text', 'text': '請輸入數字 ! ' },
-                                { 'type': 'text', 'text': '你還剩' + objStatus.arrStatus[Sta].statusTime + '機會' }]
-                            );
-                        } else if (y) {
-                            objStatus.arrStatus[Sta].statusTime--;
-                            event.reply([
-                                { 'type': 'text', 'text': '請輸入大於0的數字啦 ! ' },
-                                { 'type': 'text', 'text': '你還剩' + objStatus.arrStatus[Sta].statusTime + '機會' }]
-                            );
-                        } else {
-                            var i = objCart.arrCart[CartA].arrfood.length;
-                            var cstoreid = objCart.arrCart[CartA].storeid;
-                            var cstoreName = objCart.arrCart[CartA].storeName;
-
-                            for (var m = 0; m < i; m++) {
-                                if (objCart.arrCart[CartA].arrfood[m].foodid == objStatus.arrStatus[Sta].statusText) {
-                                    var oldQty = parseInt(objCart.arrCart[CartA].arrfood[m].foodQty);
-                                    var newQty = (oldQty + parseInt(msg1)).toString();
-                                    objCart.arrCart[CartA].arrfood[m].foodQty = newQty;
-                                    break;
-                                }
-                            }
-                            console.log(objCart.arrCart[CartA].arrfood)
-
-                            Cart.Cart(event, objCart.arrCart[CartA], userName)
-                            obj2null.status2null(objCart.arrCart[CartA], objStatus.arrStatus[Sta], CartA, Sta)
-                        }
-                    } else {
-                        var i = objCart.arrCart[CartA].arrfood.length;
-                        for (var m = 0; m < i; m++) {
-                            if (objCart.arrCart[CartA].arrfood[m].foodQty == 0) {
-
-                                objCart.arrCart[CartA].arrfood[m].length = 0
-                            }
-                        }
-                        objStatus.arrStatus[Sta].status = "";
-                        objStatus.arrStatus[Sta].statusTime = 0;
-                        objStatus.arrStatus[Sta].statusText = "";
-                        event.reply('請你閉嘴')
-                    }
-
-
+                    inputQty.inputQty(event, obj.arrPsnl[objLoc], msg1)
                 } else if (s == "changeStore") {
                     changeStore.changeStore(event, obj.arrPsnl[objLoc], msg1, msg2, lodash)
                 }
