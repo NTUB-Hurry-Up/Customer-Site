7082c4cd9ae38be179bcd71d5806cd3d51e8417f
joe890120 <joelin890120@gmail.com>
2019-05-28 20:40:15 +0800

    linebot108508

--- msg/store/storeInfo.js
+++ msg/store/storeInfo.js
@@ -18,7 +18,7 @@ var storeInfo = function (event, lodash) {
                 for (var i = 0; i < data.length; i++) {
                     arrStoreList[0].contents.contents[i] = lodash.cloneDeep(temp.temp_store_repeat)
                     console.log("imggggggggggggggggggggggggggggggggggggg-->"+data[i].storeimg)
-                    // if(data[i].storeimg == null){arrStoreList[0].contents.contents[i].hero.url = data[i].storeimg;}
+                    if(data[i].storeimg != null){arrStoreList[0].contents.contents[i].hero.url = data[i].storeimg;}
                     arrStoreList[0].contents.contents[i].body.contents[0].text = data[i].storeName;
                     arrStoreList[0].contents.contents[i].body.contents[1].contents[0].contents[1].text = data[i].storeAdd;
                     arrStoreList[0].contents.contents[i].body.contents[1].contents[1].contents[1].text = data[i].storeTel;
