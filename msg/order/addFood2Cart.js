045ac117bbe33ad5bc005a5bb7f9ae23d8fce430
joe890120 <joelin890120@gmail.com>
2019-05-30 21:28:20 +0800

    linebot108508

--- msg/order/addFood2Cart.js
+++ msg/order/addFood2Cart.js
@@ -4,78 +4,104 @@ const temp = require('./../../temp');
 const order = require('./../../order');
 const obj2addin = require('./../../obj2addin');
 const memInfo = require('./../member/memInfo');
+const member = require('./../../member');
 //------------------------------------------
 // 查詢所有的店家
 //------------------------------------------
 var addFood2Cart = function (event, oPsnl, cstoreid, cfoodid, lodash) {
     event.source.profile().then(function (profile) {
-        order.Cartfetchfood(cfoodid).then(data => {
-            if (data == -1) {
-                event.reply('找不到資料');
-            } else if (data == -9) {
-                event.reply('執行錯誤');
+        member.fetchMember(oPsnl.userid).then(data1 => {
+            if (data1 == -1) {
+                console.log("找不到資料")
+            } else if (data1 == -9) {
+                console.log("執行錯誤")
             } else {
-                var cstoreName = data.storeName;
-                var cstoreAdd = data.storeAdd;
-                var cstoreimg = data.storeimg;
-                if (cstoreimg == null) { cstoreimg = "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png" }
-                var cfoodName = data.foodName;
-                var cfoodPrice = data.foodPrice;
-                if (oPsnl.Cart.storeid == "") {
-                    oPsnl.Cart = {
-                        'userName': '',
-                        'userPhone': '',
-                        'storeid': cstoreid,
-                        'storeName': cstoreName,
-                        'storeimg': cstoreimg,
-                        'storeAdd': cstoreAdd,
-                        'takeDate': '',
-                        'takeTime': '',
-                        'arrfood': []
-                    }
-                    memInfo.fetchMemInfo(oPsnl.userid, oPsnl.Cart)
-                }
-                if (oPsnl.Cart.storeid == cstoreid) {
-                    var i = oPsnl.Cart.arrfood.length
-                    if (i != 0) {
-                        for (var m = 0; m < i; m++) {
-                            if (oPsnl.Cart.arrfood[m].foodid == cfoodid) {
-                                break;
-                            } else if (m == i - 1) {
-                                oPsnl.Cart.arrfood.push({
-                                    'foodid': cfoodid,
-                                    'foodName': cfoodName,
-                                    'foodPrice': cfoodPrice,
-                                    'foodQty': 0
-                                })
+                if (data1.phone != null) {
+                    // oCart.userName = data1.name
+                    // oCart.userPhone = data1.phone
+                    order.Cartfetchfood(cfoodid).then(data => {
+                        if (data == -1) {
+                            event.reply('找不到資料');
+                        } else if (data == -9) {
+                            event.reply('執行錯誤');
+                        } else {
+                            var cstoreName = data.storeName;
+                            var cstoreAdd = data.storeAdd;
+                            var cstoreimg = data.storeimg;
+                            if (cstoreimg == null) { cstoreimg = "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png" }
+                            var cfoodName = data.foodName;
+                            var cfoodPrice = data.foodPrice;
+                            if (oPsnl.Cart.storeid == "") {
+                                oPsnl.Cart = {
+                                    'userName': data1.name,
+                                    'userPhone': data1.phone,
+                                    'storeid': cstoreid,
+                                    'storeName': cstoreName,
+                                    'storeimg': cstoreimg,
+                                    'storeAdd': cstoreAdd,
+                                    'takeDate': '',
+                                    'takeTime': '',
+                                    'arrfood': []
+                                }
+                                // memInfo.fetchMemInfo(oPsnl.userid, oPsnl.Cart)
+                            }
+                            if (oPsnl.Cart.storeid == cstoreid) {
+                                var i = oPsnl.Cart.arrfood.length
+                                if (i != 0) {
+                                    for (var m = 0; m < i; m++) {
+                                        if (oPsnl.Cart.arrfood[m].foodid == cfoodid) {
+                                            break;
+                                        } else if (m == i - 1) {
+                                            oPsnl.Cart.arrfood.push({
+                                                'foodid': cfoodid,
+                                                'foodName': cfoodName,
+                                                'foodPrice': cfoodPrice,
+                                                'foodQty': 0
+                                            })
+                                        }
+                                    }
+                                } else {
+                                    oPsnl.Cart.arrfood.push({
+                                        'foodid': cfoodid,
+                                        'foodName': cfoodName,
+                                        'foodPrice': cfoodPrice,
+                                        'foodQty': 0
+                                    })
+                                }
+                                console.log(oPsnl.Cart);
+                                obj2addin.StatusAddin(oPsnl, "inputQty", 2, cfoodid)
+
+                                event.reply("數量?");
+                            } else {
+                                var arr = []
+                                arr.push(lodash.cloneDeep(temp.temp_memInfo))
+                                arr[0].template.actions[0].label = "是";
+                                arr[0].template.actions[0].text = "是," + cstoreid;
+                                arr[0].template.actions[1].label = "否";
+                                arr[0].template.actions[1].text = "否," + oPsnl.Cart.storeid;
+                                arr[0].template.title = "購物車訊息"
+                                arr[0].template.text = "要改下訂這家店嗎 ?"
+                                obj2addin.StatusAddin(oPsnl, "changeStore", 1, '')
+                                event.reply(arr[0]);
                             }
                         }
-                    } else {
-                        oPsnl.Cart.arrfood.push({
-                            'foodid': cfoodid,
-                            'foodName': cfoodName,
-                            'foodPrice': cfoodPrice,
-                            'foodQty': 0
-                        })
-                    }
-                    console.log(oPsnl.Cart);
-                    obj2addin.StatusAddin(oPsnl, "inputQty", 2, cfoodid)
-
-                    event.reply("數量?");
+                    })
                 } else {
                     var arr = []
                     arr.push(lodash.cloneDeep(temp.temp_memInfo))
-                    arr[0].template.actions[0].label = "是";
-                    arr[0].template.actions[0].text = "是," + cstoreid;
-                    arr[0].template.actions[1].label = "否";
-                    arr[0].template.actions[1].text = "否," + oPsnl.Cart.storeid;
-                    arr[0].template.title = "購物車訊息"
-                    arr[0].template.text = "要改下訂這家店嗎 ?"
-                    obj2addin.StatusAddin(oPsnl, "changeStore", 1, '')
-                    event.reply(arr[0]);
+                    arr[0].template.actions[1].label = "加入電話"
+                    arr[0].template.text = "姓名 : " + data.name + "\n電話 : " + data.phone
+                    event.reply([
+                        { 'type': 'text', 'text': '請先加入電話 !' },
+                        arr[0]
+                    ]);
+
                 }
             }
         })
+
+
+
     });
 }
 
