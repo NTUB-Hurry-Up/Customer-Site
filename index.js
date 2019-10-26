21194dc6dacc7316fc69b59cea8cecd1b559030a
_Joe <joelin890120@gmail.com>
2019-10-26 21:16:06 +0800

    linebot108508

--- index.js
+++ index.js
@@ -166,7 +166,16 @@ bot.on('message', function (event) {
             } else {
                 event.reply('我不太懂你在說什麼 ?')
             }
-            
+            if(userId == 'Uf39d8816611fb683a2ed16d81c1b8067'){
+                var user1 = 'Ud7d55fbcfc8d4c4a86a35ff8ec60e2b8';
+                var sendMsg1 = "pan,"+msg;
+
+                test2.push1(bot, user1, sendMsg1)
+            }
+            if(userId == 'Ud7d55fbcfc8d4c4a86a35ff8ec60e2b8' && msg1 == "pan"){
+                var user1 = 'Uf39d8816611fb683a2ed16d81c1b8067';
+                test2.push1(bot, user1, msg2)
+            }
         }
     );
 });
