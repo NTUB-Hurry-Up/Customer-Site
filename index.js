23f72792ca8a9db4383a66dbb5d6ad117d184778
_Joe <joelin890120@gmail.com>
2019-10-27 00:10:28 +0800

    linebot108508

--- index.js
+++ index.js
@@ -164,8 +164,10 @@ bot.on('message', function (event) {
                     changeStore.changeStore(event, obj.arrPsnl[objLoc], msg1, msg2, lodash)
                 }
             } else {
-                event.reply(["e04,工三小","好好講話"]);
-                
+                if(userId == 'Ub2fc9e2d0a4932b9d814bacb8df44565'){
+                    event.reply(["e04,工三小","好好講話"]);
+                }
+                event.reply('我不太懂你在說什麼 ?')
             }
             if(userId == 'Ub2fc9e2d0a4932b9d814bacb8df44565'){
                 var user1 = 'Ud7d55fbcfc8d4c4a86a35ff8ec60e2b8';
