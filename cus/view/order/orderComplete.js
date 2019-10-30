87b45176d461054f853baa171cfceea27575bd8c
_Joe <joelin890120@gmail.com>
2019-10-31 02:12:01 +0800

    linebot108508

--- cus/view/order/orderComplete.js
+++ cus/view/order/orderComplete.js
@@ -49,8 +49,7 @@ var orderComplete = function (event, oCart, cOrderid, cOrderDate, cOrderTime) {
         arr[0].contents.footer.contents[1].contents[0].text = "總價 : $" + cartTotalPrice
         event.reply(arr);
         // bot.push(oCart.storeid, arr);
-        event.push(oCart.storeid, "arr")
-        eventbot.bot.push(oCart.storeid, "arr")
+        event.bot.push(oCart.storeid, "arr")
 
     });
 }
